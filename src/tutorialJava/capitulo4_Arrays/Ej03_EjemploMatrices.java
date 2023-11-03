6375df7d5c456b292a75b475f8cce26b2d812c56
Rafael Muñoz <rafael.munoz77@gmail.com>
2023-11-03 14:01:19 +0100

    feat (ch 4): matrix exammples added

--- src/tutorialJava/capitulo4_Arrays/Ej03_EjemploMatrices.java
+++ src/tutorialJava/capitulo4_Arrays/Ej03_EjemploMatrices.java
@@ -5,20 +5,23 @@ import tutorialJava.Utils;
 public class Ej03_EjemploMatrices {
 
 	public static void main(String[] args) {
-		// Declaración
-		int m[][] = new int[10][10];
+		int m[][] = new int[4][3];
+		int m2[][] = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
 		
-		// Incialización
+		int cont = 1;
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
-				m[i][j] = Utils.obtenerNumeroAzar(0, 100);
+//				m[i][j] = (int) Math.round(Math.random() * 100);
+				m[i][j] = cont;
+				cont++;
 			}
 		}
 		
-		// Salida por consola
+		m[0][2] = 1;
+		
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
-				System.out.print(m[i][j] + "\t");
+				System.out.print(m[i][j] + " ");
 			}
 			System.out.println();
 		}
--- src/tutorialJava/capitulo4_Arrays/Ej04_ComoHacerEjerciciosMatrices.java
+++ src/tutorialJava/capitulo4_Arrays/Ej04_ComoHacerEjerciciosMatrices.java
@@ -1,5 +1,7 @@
 package tutorialJava.capitulo4_Arrays;
 
+import java.util.Iterator;
+
 import tutorialJava.Utils;
 
 public class Ej04_ComoHacerEjerciciosMatrices {
@@ -9,94 +11,35 @@ public class Ej04_ComoHacerEjerciciosMatrices {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int a[] = new int[] {1, 2, 3, 4, 5};
 		int m[][] = new int[][] { 	{1, 	2, 		3, 		4, 		0}, 
 									{5, 	6, 		0, 		8, 		9}, 
 									{10,	0, 		12, 	13, 	14}, 
 									{0, 	16,		17, 	18, 	19}, 
 									{20, 	21, 	22, 	0, 		24} };
-//		int m[][] = new int[5][5];
-		
-//		inicializaMatriz(m);
+									
+//		inicializaMatrizAlAzar(m);
 		imprimeMatriz(m);
-		System.out.println("Es dispersa: " + esmatrizDispersa(m));
+		System.out.println("Es positiva: " + esMatrizPositiva(m));
 	}
 	
 
-	public static boolean esmatrizDispersa (int m[][]) {
-		boolean dispersa = true;
-		for (int i = 0; i < m.length; i++) {
-			boolean hayCerosEnEstaFila = false;
-			for (int j = 0; j < m[i].length; j++) {
-				if (m[i][j] == 0) {
-					hayCerosEnEstaFila = true;
-				}
-			}
-			// Acabo de recorrer una fila
-			if (hayCerosEnEstaFila == false) {
-				dispersa = false;
-			}
-		}
-		return dispersa;
-	}
-	
-	
-	
-	public static boolean esmatrizDispersa2 (int m[][]) {
-		// Recorro las filas buscando una que no tenga ceros
-		for (int i = 0; i < m.length; i++) {
-			boolean hayCerosEnEstaFila = false;
-			for (int j = 0; j < m[i].length; j++) {
-				// Si encuentro un elemento que sea cero lo registro
-				if (m[i][j] == 0) { hayCerosEnEstaFila = true; }
-			}
-			// Acabo de recorrer una fila
-			if (!hayCerosEnEstaFila) {
-				return false;
-			}
-		}
-		
-		// Recorro las columnas buscando una que no tenga ceros
-		for (int i = 0; i < m[0].length; i++) {
-			boolean hayCerosEnEstaColumna = false;
-			for (int j = 0; j < m.length; j++) {
-				// Si encuentro un elemento que sea cero lo registro
-				if (m[i][j] == 0) { hayCerosEnEstaColumna = true; }
-			}
-			// Acabo de recorrer una fila
-			if (!hayCerosEnEstaColumna) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	
-	
-	public static boolean esMatrizPositiva (int n[][]) {
-		for (int i = 0; i < n.length; i++) {
-			for (int j = 0; j < n[i].length; j++) {
-				if (n[i][j] < 0) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-
-	
-
-	public static void inicializaMatriz(int m[][]) {
+	/**
+	 * 
+	 * @param m
+	 */
+	public static void inicializaMatrizAlAzar(int m[][]) {
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
-				m[i][j] = Utils.obtenerNumeroAzar();
+				m[i][j] = (int) Math.round(Math.random() * 100);
 			}
 		}
 	}
 	
-	
-	public static void imprimeMatriz (int m[][]) {
+	/**
+	 * 
+	 * @param m
+	 */
+	public static void imprimeMatriz(int m[][]) {
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
 				System.out.print(m[i][j] + "\t");
@@ -106,4 +49,36 @@ public class Ej04_ComoHacerEjerciciosMatrices {
 	}
 	
 	
+	/**
+	 * 
+	 * @param m
+	 * @return
+	 */
+	public static boolean esMatrizPositiva(int m[][]) {
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
+				if (m[i][j] < 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
