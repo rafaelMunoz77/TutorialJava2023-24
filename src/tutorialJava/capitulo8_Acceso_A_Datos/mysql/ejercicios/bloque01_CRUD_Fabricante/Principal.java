0de36e15895037a37debc917f13cd85fd72648f6
Rafa Muñoz <rafael.munoz77@gmail.com>
2024-02-12 10:42:17 +0100

    feat(ch 8 mysql): ex 1 solved

---
+++ src/tutorialJava/capitulo8_Acceso_A_Datos/mysql/ejercicios/bloque01_CRUD_Fabricante/Principal.java
@@ -0,0 +1,65 @@
+package tutorialJava.capitulo8_Acceso_A_Datos.mysql.ejercicios.bloque01_CRUD_Fabricante;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int opcion = 0;
+		
+		do {
+			opcion = menu();
+
+			switch(opcion) {
+			case 1: // Listado de fabricantes
+				GestionFabricante.listarFabricantes();
+				break;
+			case 2: // Nuevo fabricante
+				GestionFabricante.nuevoFabricante();
+				break;
+			case 3: // Modificar fabricante
+				GestionFabricante.modificarFabricante();
+				break;
+			case 4: // Eliminar fabricante
+				GestionFabricante.eliminarFabricante();
+				break;
+			}
+
+		} while (opcion != 0);
+		
+
+	}
+	
+	
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static int menu () {
+		Scanner sc = new Scanner(System.in);
+		int opcionElegida = 0;
+		
+		System.out.println("\n\nMenú" + 
+				"\n\t1.- Listar Fabricantes" +
+				"\n\t2.- Nuevo Fabricante" +
+				"\n\t3.- Modificar Fabricante" + 
+				"\n\t4.- Eliminar Fabricante" +
+				"\nIntroduzca su opción: ");
+		
+		opcionElegida = sc.nextInt();
+		return opcionElegida;
+	}
+}
---
+++ src/tutorialJava/capitulo8_Acceso_A_Datos/mysql/ejercicios/bloque01_CRUD_Fabricante/SupertipoGestion.java
@@ -0,0 +1,30 @@
+package tutorialJava.capitulo8_Acceso_A_Datos.mysql.ejercicios.bloque01_CRUD_Fabricante;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SupertipoGestion {
+
+	
+	/**
+	 * 
+	 * @param nombreTabla
+	 * @return
+	 */
+	protected static int maxIdEnTabla(String nombreTabla) {
+		try {
+			Statement s = ConnectionManager.getConexion().createStatement();
+			ResultSet rs = s.executeQuery("Select max(id) from " + nombreTabla);
+			
+			if (rs.next()) {
+				return rs.getInt(1) + 1;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return -1; 		
+	}
+
+}
